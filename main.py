0f8daca16da53cde854bae705f5ebaccfc1b34c3
Jose Briones <josmbrio@gmail.com>
2022-12-15 01:38:02 -0500

    Migration from local to gitlab

---
+++ main.py
@@ -0,0 +1,40 @@
+import boto3
+
+ec2_client = boto3.client('ec2', region_name="eu-central-1")
+ec2_resource = boto3.resource('ec2')
+
+new_vpc = ec2_resource.create_vpc(
+    CidrBlock = "10.0.0.0/16"
+)
+
+new_vpc.create_subnet(
+    CidrBlock = "10.0.1.0/24"
+)
+
+new_vpc.create_subnet(
+    CidrBlock = "10.0.2.0/24"
+)
+
+new_vpc.create_tags(
+    Tags = [
+        {
+            'Key' : 'Name',
+            'Value' : 'my-vpc'
+        }
+    ]
+)
+
+all_available_vpcs = ec2_client.describe_vpcs()
+vpcs = all_available_vpcs["Vpcs"]
+#print(vpcs)
+#print(type(vpcs))
+
+for vpc in vpcs:
+    print(vpc["VpcId"])
+    cidr_block_assoc_sets = vpc["CidrBlockAssociationSet"]
+    #print(cidr_block_assoc_sets)
+    for assoc_set in cidr_block_assoc_sets:
+        print(assoc_set['CidrBlockState'])
+
+
+
